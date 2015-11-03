dd007b9646f12aa80e860f454c1f0f53f4e1e92a
Francis Massey <francismasse_2@hotmail.com>
2015-11-02 19:40:46 -0500

    Adding open/release probe/disconnect functions, not tested

--- usbcam.c
+++ usbcam.c
@@ -1,21 +1,24 @@
 /*
  * File         : usbcam.c
- * Description  : ELE784 Lab2 source
+ * Description  : ELE784 Lab1 source
  *
- * Etudiants:  XXXX00000000 (prenom nom #1)
- *             XXXX00000000 (prenom nom #2)
+ * Etudiants:  MASF05089000 (Francis Masse)
+ *             LEBA23057609 (Alexandre Leblanc)
  */
 
+#include "usbcam.h"
 #include "usbvideo.h"
 #include "dht_data.h"
 
+#define __DEBUG__
+
 // Module Information
-MODULE_AUTHOR("prenom nom #1, prenom nom #2");
+MODULE_AUTHOR("Francis Masse, Alexandre Leblanc");
 MODULE_LICENSE("Dual BSD/GPL");
 
 #define DEVICE_NAME "etsele_cdev"
 
-// Private function prototypes
+// Helper function
 static int urbInit(struct urb *urb, struct usb_interface *intf);
 static void urbCompletionCallback(struct urb *urb);
 
@@ -26,50 +29,222 @@ static void urbCompletionCallback(struct urb *urb);
 //static char * myData;
 //static struct urb *myUrb[5];
 
+// **************************** //
+// ***** Helper functions ***** //
+// **************************** //
+
+static void usbcam_delete(struct kref *kref)
+{
+  struct usbcam_dev *dev = container_of(kref, struct usbcam_dev, kref);
+
+  usb_put_dev(dev->usbdev);
+  kfree (dev->bulk_in_buffer);
+  kfree (dev);
+}
+
+static struct usbcam_dev usbcam;
 
 static int __init usbcam_init(void)
 {
-    return 0;
+  int result;
+
+  result = usb_register(&usbcam_driver);
+#ifdef __DEBUG__
+  if(result)
+    printk(KERN_WARNING "usb_register failed. Error number %d", result);
+#endif
+
+  sema_init(&usbcam.usbSem, 1);
+
+#ifdef __DEBUG__
+  printk(KERN_WARNING "====================================================\n");
+  printk(KERN_WARNING "====================================================\n");
+  printk(KERN_WARNING "                Device initialized\n");
+  printk(KERN_WARNING "====================================================\n");
+  printk(KERN_WARNING "====================================================\n");
+#endif
+
+  return 0;
 }
 
 static void __exit usbcam_exit(void)
 {
+  usb_deregister(&usbcam_driver);
 
+#ifdef __DEBUG__
+  printk(KERN_WARNING "====================================================\n");
+  printk(KERN_WARNING "====================================================\n");
+  printk(KERN_WARNING "                Device released\n");
+  printk(KERN_WARNING "====================================================\n");
+  printk(KERN_WARNING "====================================================\n");
+#endif
 }
 
-static int usbcam_probe(struct usb_interface *intf, const struct usb_device_id *devid)
+static int usbcam_probe(struct usb_interface *interface, const struct usb_device_id *devid)
 {
-    return -1;
+  struct usbcam_dev *dev = NULL;
+  struct usb_host_interface *iface_desc;
+  struct usb_endpoint_descriptor *endpoint;
+  size_t buffer_size;
+  int i;
+  int retval = -ENOMEM;
+
+  /* allocate memory for our device state and initialize it */
+  dev = kmalloc(sizeof(struct usbcam_dev), GFP_KERNEL);
+  if(dev == NULL)
+  {
+    printk(KERN_WARNING "Out of memory");
+    goto error;
+  }
+  memset(dev, 0x00, sizeof(*dev));
+  kref_init(&dev->kref);
+
+  dev->usbdev = usb_get_dev(interface_to_usbdev(interface));
+  dev->usbinterfaces = interface;
+
+  /* set up the endpoint information */
+  /* use only the first bulk-in and bulk-out endpoints */
+  iface_desc = interface->cur_altsetting;
+  for (i = 0; i < iface_desc->desc.bNumEndpoints; ++i)
+  {
+    endpoint = &iface_desc->endpoint[i].desc;
+
+    if (!dev->bulk_in_endpointAddr && (endpoint->bEndpointAddress & USB_DIR_IN) && ((endpoint->bmAttributes & USB_ENDPOINT_XFERTYPE_MASK) == USB_ENDPOINT_XFER_BULK))
+    {
+      /* we found a bulk in endpoint */
+      buffer_size = endpoint->wMaxPacketSize;
+      dev->bulk_in_size = buffer_size;
+      dev->bulk_in_endpointAddr = endpoint->bEndpointAddress;
+      dev->bulk_in_buffer = kmalloc(buffer_size, GFP_KERNEL);
+      if (!dev->bulk_in_buffer)
+      {
+        printk(KERN_WARNING "Could not allocate bulk_in_buffer");
+        goto error;
+      }
+    }
+
+    if (!dev->bulk_out_endpointAddr && !(endpoint->bEndpointAddress & USB_DIR_IN) && ((endpoint->bmAttributes & USB_ENDPOINT_XFERTYPE_MASK) == USB_ENDPOINT_XFER_BULK))
+    {
+      /* we found a bulk out endpoint */
+      dev->bulk_out_endpointAddr = endpoint->bEndpointAddress;
+    }
+  }
+  if (!(dev->bulk_in_endpointAddr && dev->bulk_out_endpointAddr))
+  {
+    printk(KERN_WARNING "Could not find both bulk-in and bulk-out endpoints");
+    goto error;
+  }
+
+  /* save our data pointer in this interface device */
+  usb_set_intfdata(interface, dev);
+
+  /* we can register the device now, as it is ready */
+  retval = usb_register_dev(interface, &usbcam_class);
+  if (retval)
+  {
+    /* something prevented us from registering this driver */
+    printk(KERN_WARNING"Not able to get a minor for this device.");
+    usb_set_intfdata(interface, NULL);
+    goto error;
+  }
+
+  /* let the user know what node this device is now attached to */
+  printk(KERN_WARNING "usbcam device now attached to usbcam-%d", interface->minor);
+  return 0;
+
+  error:
+  if (dev)
+    kref_put(&dev->kref, usbcam_delete);
+
+  return retval;
 }
 
-void usbcam_disconnect(struct usb_interface *intf)
+void usbcam_disconnect(struct usb_interface *interface)
 {
+  struct usbcam_dev *dev;
+  int minor = interface->minor;
 
+  sema_init(&dev.usbSem, 1);
+
+  if(down_interruptible(&dev->usbSem))
+
+  dev = usb_get_intfdata(interface);
+  usb_set_intfdata(interface, NULL);
+
+  /* give back our minor */
+  usb_deregister_dev(interface, &usbcam_class);
+
+  up(&dev->usbSem);
+
+  /* decrement our usage count */
+  kref_put(&dev->kref, usbcam_delete);
+
+  printk(KERN_WARNING "usbcam-%d now disconnected", minor);
 }
 
 int usbcam_open(struct inode *inode, struct file *filp)
 {
-    return 0;
+  struct usbcam_dev *dev;
+  struct usb_interfaces *interface;
+  int subminor;
+  int retval = 0;
+
+  subminor = iminor(inode);
+
+  interface = usb_find_interface(&usbcam_driver, subminor);
+  if(interface == NULL)
+  {
+    printk(KERN_WARNING "%s - error, can't find device for minor %d", __FUNCTION__, subminor);
+    retval = -ENODEV;
+    goto exit;
+  }
+
+  dev = usb_get_intfdata(interface);
+  if(dev == NULL)
+  {
+    retval = -ENODEV;
+    goto exit;
+  }
+
+  /* increment our usage count for the device */
+  kref_get(&dev->kref);
+
+  /* save our object in the file's private structure */
+  filp->private_data = dev;
+
+  exit:
+  return retval;
 }
 
 int usbcam_release(struct inode *inode, struct file *filp)
 {
-    return 0;
+  struct usbcam_dev *dev;
+
+  dev = (struct usbcam_dev *)filp->private_data;
+  if (dev == NULL)
+    return -ENODEV;
+
+  /* decrement the count on our device */
+  kref_put(&dev->kref, usbcam_delete);
+  return 0;
 }
 
 ssize_t usbcam_read(struct file *filp, char __user *ubuf, size_t count, loff_t *f_ops)
 {
-    return 0;
+
+  return 0;
 }
 
 ssize_t usbcam_write(struct file *filp, const char __user *ubuf, size_t count, loff_t *f_ops)
 {
-    return 0;
+
+  return 0;
 }
 
 long usbcam_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 {
-    return 0;
+
+  return 0;
 }
 
 module_init(usbcam_init);
--- usbvideo.h
+++ usbvideo.h
@@ -9,27 +9,6 @@
  *
  *****************************************************************************/
 
-#include <linux/module.h>
-#include <linux/init.h>
-#include <linux/kernel.h>
-#include <linux/cdev.h>
-#include <linux/fs.h>
-#include <linux/types.h>
-#include <linux/slab.h>
-#include <linux/errno.h>
-#include <linux/sched.h>
-#include <uapi/asm-generic/errno-base.h>
-#include <linux/fcntl.h>
-#include <uapi/asm-generic/fcntl.h>
-#include <linux/wait.h>
-#include <linux/spinlock.h>
-#include <linux/device.h>
-#include <asm/atomic.h>
-#include <asm/uaccess.h>
-#include <uapi/asm-generic/ioctl.h>
-#include <linux/usb.h>
-#include <linux/completion.h>
-
 /*****************************************************************************
  *      Definitions based on USB_Video_Class_1.1.pdf (www.usb.org)
  *****************************************************************************/
@@ -199,52 +178,3 @@
 #define STATE_DISABLED                             0x04
 #define AUTOUPDATE_CONTROL                         0x08
 #define ASYNCHRONOUS_CONTROL                       0x10
-
-// Prototypes
-static int __init usbcam_init(void);
-static void __exit usbcam_exit(void);
-static int usbcam_probe(struct usb_interface *intf, const struct usb_device_id *devid);
-static void usbcam_disconnect(struct usb_interface *intf);
-static int usbcam_open(struct inode *inode, struct file *filp);
-static int usbcam_release(struct inode *inode, struct file *filp) ;
-static ssize_t usbcam_read(struct file *filp, char __user *ubuf, size_t count, loff_t *f_ops);
-static ssize_t usbcam_write(struct file *filp, const char __user *ubuf, size_t count, loff_t *f_ops);
-static long usbcam_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
-
-struct usbcam_dev {
-  struct usb_device *usbdev;
-};
-
-static struct usb_device_id usbcam_table[] = {
-// { USB_DEVICE(VENDOR_ID, PRODUCT_ID) },
-{ USB_DEVICE(0x046d, 0x08cc) },
-{}
-};
-
-MODULE_DEVICE_TABLE(usb, usbcam_table);
-// USB Driver structure
-static struct usb_driver usbcam_driver = {
-  .name       = "usbcam",
-  .id_table   = usbcam_table,
-  .probe      = usbcam_probe,
-  .disconnect = usbcam_disconnect,
-};
-
-MODULE_DEVICE_TABLE(usb, usbcam_table);
-
-// File operation structure
-struct file_operations usbcam_fops = {
-  .owner          = THIS_MODULE,
-  .open           = usbcam_open,
-  .release        = usbcam_release,
-  .read           = usbcam_read,
-  .write          = usbcam_write,
-  .unlocked_ioctl = usbcam_ioctl,
-};
-
-#define USBCAM_MINOR 0
-static struct usb_class_driver usbcam_class = {
-  .name       = "usb/usbcam%d",
-  .fops       = &usbcam_fops,
-  .minor_base = USBCAM_MINOR,
-};
